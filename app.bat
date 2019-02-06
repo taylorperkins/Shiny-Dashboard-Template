1a3df308f68e052fb8e972562481fd574f006bae
LaranIkal <laran.ikal@gmail.com>
2019-02-06 10:39:56 -0600

    Add batch and shell files
    I created batch and shell files to run the app from command line on Windows and Linux

---
+++ app.bat
@@ -0,0 +1,7 @@
+ECHO OFF
+CLS 
+REM  Run Rscript with path argument
+C:\R\R-3.5.2\bin\Rscript app.R %cd%
+
+
+
---
+++ app.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+# Set actual path variable
+actualpath=$(pwd -P)
+
+# Run Rscript with local path argument
+Rscript app.R $scriptpath
+
+
