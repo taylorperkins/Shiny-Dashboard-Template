6fcc9778ef837c4f69493a401941bca68a8f9053
LaranIkal <laran.ikal@gmail.com>
2019-02-06 11:07:24 -0600

    Read arguments and set working directory path
    Get actual directory path from args passed by the shel/batch file and set the working directory.

--- app.R
+++ app.R
@@ -4,6 +4,11 @@
 # Main controller. 
 # Used to import your ui and server components; initializes the app.
 ###################
+
+args <- commandArgs( trailingOnly = TRUE ) # read arguments
+
+setwd( args[1] ) # Get actual directory path from args passed by the shel/batch file.
+
 library(shiny)
 library(shinydashboard)
 
